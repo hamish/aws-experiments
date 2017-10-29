f76b6f3fffaca55e93e198d653314dd1f86c2c9e
Hamish Currie <hamish@currie.com.au>
2017-10-29 14:09:34 +1000

    pass command line args through

--- runExperiment.sh
+++ runExperiment.sh
@@ -1,5 +1,5 @@
 if command -v pipenv 2>/dev/null; then
-    pipenv run python runExperiment.py
+    pipenv run python runExperiment.py "$@"
 else
     echo "install pipenv before using: pip install pipenv"
 fi
